e8ffe7d26778a9d425dd3696ded291d7911169a4
kr8n3r <jani.kraner@digital.cabinet-office.gov.uk>
2020-06-11 09:45:48 +0100

    Add 'js-enabled' class to document body

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
+import React, { useEffect } from 'react';
 import '../styles/application.scss'
 
 export default function ProductPages({ Component, pageProps }) {
+  useEffect(() => {
+    window.document.body.classList.add('js-enabled');
+  }, []);
   return <Component {...pageProps} />
 }
